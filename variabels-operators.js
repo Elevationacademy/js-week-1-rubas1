05375f594bae28c29a2b9257e19e4b190e1cafa3
ruba <rubsaleh1@gmail.com>
2020-10-27 17:09:57 +0200

    week 1 exercises

---
+++ variabels-operators.js
@@ -0,0 +1,21 @@
+//////////////Exercise 1//////////////
+const maxCapacity = 14
+let surfTime = true
+let bestStudent
+const purposeInLife //ERROR: MUST USE LET
+console.log(maxCapacity)
+console.log(surfTime)
+console.log(bestStudent)
+console.log(purposeInLife)
+
+//////////////Exercise 2//////////////
+const myVar = "MY dog "
+const result = myVar + "Is the best thing ever"
+console.log(result)
+
+//////////////Exercise 3//////////////
+const password = "ruba123"
+const confirmPassword = "ruba124"
+console.log(password==confirmPassword)
+
+
